9383c93704ff2c7ee9b2120c9f02a591332a387c
Takashi ITO <cortyuming@gmail.com>
2013-10-12 13:31:14 +0900

    閉区間を生成のクラスと下端点取得のテストを追加する

---
+++ closed_range.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# *-# -*- coding: utf-8 -*-
+
+class ClosedRange:
+    def __init__(self, lower_endpoint, upper_endpoint):
+        self.lower_endpoint = lower_endpoint
+        self.upper_endpoint = upper_endpoint
+
+    def get_lower_endpoint(self):
+        return self.lower_endpoint
---
+++ closed_range_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# *-# -*- coding: utf-8 -*-
+
+# 下端点と上端点を与えて閉区間を生成しよう
+# 閉区間から下端点と上端点を取得しよう
+# 閉区間から文字列表記を取得しよう
+
+from closed_range import ClosedRange
+
+def test_create_range():
+    range_interval = ClosedRange(3, 8)
+    assert range_interval.get_lower_endpoint() == 3
