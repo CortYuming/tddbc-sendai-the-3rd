d17eb0477ad2cbb43801a28dcba89b7c46b8ae30
Takuya Nishigori <nishigori.tak@gmail.com>
2013-10-12 14:07:23 +0900

    リファクタ 閉区間の文字列表記変換のテストメソッドを切り分け, 不要なコメントを外した

--- closed_range_test.py
+++ closed_range_test.py
@@ -9,17 +9,18 @@ from pytest import fail
 from closed_range import ClosedRange
 
 
-
 def test_create_range():
     range_interval = ClosedRange(3, 8)
     assert range_interval.get_lower_endpoint() == 3
     assert range_interval.get_upper_endpoint() == 8
+
+def test_convert_able_to_string():
+    range_interval = ClosedRange(3, 8)
     assert range_interval.toString() == "[3,8]"
 
-def test_throw_except_initiral():
-    ClosedRange(8, 3)
-    # try:
-    #     ClosedRange(8, 3)
-    #     fail("not throwing")
-    # except:
-    #     pass
+def test_throw_except_initial():
+    try:
+        ClosedRange(8, 3)
+        fail("not throwing")
+    except:
+        pass
